1055e14d5d9de3184777397efa6245afb76e0e3b
Darren <bhl865931168@gmail.com>
2019-08-20 14:28:58 +0800

    Update 138. Copy List with Random Pointer

--- LeetCode/Copy List with Random Pointer.cpp
+++ LeetCode/Copy List with Random Pointer.cpp
@@ -20,54 +20,21 @@ public:
     Node* copyRandomList(Node* head) {
         if (!head)
             return NULL;
-        unordered_map<int,Node*> m;
+        map<Node*,Node*> m;
         Node* cur=head;
         while (cur) {
-            auto it=m.find(cur->val);
-            Node* nptr;
-            if (it==m.end()) {
-                nptr=new Node();
-                nptr->val=cur->val;
-                m[nptr->val]=nptr;
-            }
-            else {
-                nptr=it->second;
-            }
-            if (cur->next) {
-                it=m.find(cur->next->val);
-                Node* nextptr;
-                if (it==m.end()) {
-                    nextptr=new Node();
-                    nextptr->val=cur->next->val;
-                    m[nextptr->val]=nextptr;
-                }
-                else {
-                    nextptr=it->second;
-                }
-                nptr->next=nextptr;
-            }
-            else {
-                nptr->next=NULL;
-            }
-            if (cur->random) {
-                it=m.find(cur->random->val);
-                Node* ranptr;
-                if (it==m.end()) {
-                    ranptr=new Node();
-                    ranptr->val=cur->random->val;
-                    m[ranptr->val]=ranptr;
-                }
-                else {
-                    ranptr=it->second;
-                }
-                nptr->random=ranptr;
-            }
-            else {
-                nptr->random=NULL;
-            }
+            Node* nptr=new Node();
+            nptr->val=cur->val;
+            m[cur]=nptr;
             cur=cur->next;
         }
-        auto it=m.find(head->val);
-        return it->second;
+        cur=head;
+        while (cur) {
+            Node* n=m[cur];
+            n->next=cur->next?m[cur->next]:NULL;
+            n->random=cur->random?m[cur->random]:NULL;
+            cur=cur->next;
+        }
+        return m[head];
     }
 };
