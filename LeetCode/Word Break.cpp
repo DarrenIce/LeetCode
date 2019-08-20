acf53c1662573c26e7f7955f058433dcfc3cab59
Darren <bhl865931168@gmail.com>
2019-08-20 15:10:18 +0800

    Update Word Break.cpp

--- LeetCode/Word Break.cpp
+++ LeetCode/Word Break.cpp
@@ -5,10 +5,7 @@ public:
         dp[0]=true;
         for (int i=1;i<=s.size();i++) {
             for (auto word:wordDict) {
-                if (i<word.size())
-                    dp[i]=dp[i]||false;
-                else
-                    dp[i]=dp[i]||(s.substr(i-word.size(),word.size())==word&&dp[i-word.size()]);
+                dp[i]=dp[i]||(i>=word.size()?(s.substr(i-word.size(),word.size())==word&&dp[i-word.size()]):false);
             }
         }
         return dp[s.size()];
